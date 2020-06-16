74d20b9a84a30fd8a18f9507686f371410013b20
ardiansah21 <email.ardi21@gmail.com>
2020-06-16 14:50:33 +0700

    tes menambah halaman member

---
+++ app/Http/Controllers/MemberController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MemberController extends Controller
+{
+    public function profil(){
+        return "Halo ini adalah method profil, dalam controller MemberController";
+    }
+}
