c9bc0951f0b87a3253ec78e699c644ad24962a22
ardiansah <email.ardi21@gmail.com>
2020-06-22 00:55:12 +0700

    temp

---
+++ database/seeds/MemberSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+
+class MemberSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // data faker indonesia
+        $faker = Faker::create('id_ID');
+
+        // membuat data dummy sebanyak 10 record
+        for($x = 1; $x <= 3; $x++){
+
+        	// insert data dummy pegawai dengan faker
+        	DB::table('members')->insert([
+        		'nama_lengkap' => $faker->name,
+        		'email' => $faker->email,
+        		'password' => $faker->password,
+                'jumlah_saldo' => $faker->randomDigit,
+                'nomor_hp'=>$faker->randomNumber($nbDigits = NULL, $strict = false),
+                'jenis_kelamin'=>$faker->randomElement($array = array ('L','P')),
+                'foto_profil'=>null,
+                'foto_profil'=>null,
+                'produk_favorit'=>null,
+        	]);
+        }
+    }
+}
