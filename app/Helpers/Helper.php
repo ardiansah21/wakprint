8d286f47376394b22912acc3f9cfc5185dbb208f
imaha7 <ilham.maulana.07.0698@gmail.com>
2020-09-23 21:59:59 +0700

    temp

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -20,7 +20,7 @@ function set_active($uri, $output = 'active')
 
 function cekWarna($path, $percenMinimum = 0)
 {
-    ini_set('max_execution_time', 3000);
+    ini_set('max_execution_time', 3000000);
     $start = microtime(true);
 
     $totalPageGray = 0;
