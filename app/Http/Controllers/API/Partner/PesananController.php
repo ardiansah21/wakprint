53958a1bbfa3f584f613f95e6ca97eb712565071
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-01-05 15:51:28 +0700

    temp

--- app/Http/Controllers/API/Partner/PesananController.php
+++ app/Http/Controllers/API/Partner/PesananController.php
@@ -35,7 +35,7 @@ class PesananController extends Controller
                 "jumlah_file" => $jumlahFile,
                 "pesanan" => request()->user()->pesanans->where('status', 'Pending'),
             ];
-            return responseSuccess("data pesanan masuk partner yang login", collect($data));
+            return responseSuccess("data pesanan masuk partner yang login", collect($jumlahFile, request()->user()->pesanans->where('status', 'Pending')));
         } else {
             return responseError("data pesanan masuk partner tidak ada");
         }
