88d7e057dcc2fa75c33ecf6ff07a0e8f4162f38e
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-01-11 15:09:17 +0700

    temp

--- app/Http/Controllers/API/Partner/PromoController.php
+++ app/Http/Controllers/API/Partner/PromoController.php
@@ -68,7 +68,7 @@ class PromoController extends Controller
             return responseError('Maaf waktu mulai promo tidak boleh melewati masa waktu selesai promo, silahkan periksa kembali yah');
         }
 
-        foreach ($request->id_produk as $id) {
+        foreach (array($request->id_produk) as $id) {
             $produk = $partner->products->find($id);
             $produk->status_diskon = $statusDiskon;
             $produk->maksimal_diskon = (int) str_replace('.', '', $maksimalDiskon);
