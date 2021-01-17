f1b574b2818e56d36469550366f9c02962059bc4
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-01-17 22:58:28 +0700

    temp

--- app/Http/Controllers/API/Member/MemberController.php
+++ app/Http/Controllers/API/Member/MemberController.php
@@ -61,7 +61,7 @@ class MemberController extends Controller
         } else {
             if ($member) {
                 $current_password = $member->password;
-                if (Hash::check($request->password_baru, $current_password)) {
+                if (Hash::check(Hash::make($request->password_baru), $current_password)) {
                     $member->update([
                         'nama_lengkap' => $request->nama,
                         'jenis_kelamin' => $request->jk,
