92b9df05856404e89b6bd783833feeac2ed84e70
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-01-17 15:56:01 +0700

    temp

--- app/Produk.php
+++ app/Produk.php
@@ -65,7 +65,7 @@ class Produk extends Model implements HasMedia
         if (!empty($this->getFirstMediaUrl('foto_produk'))) {
             return 'https://wakprint.com' . $this->getFirstMediaUrl('foto_produk');
         }
-        return 'https://ui-avatars.com/api/?name=' . trim($this->nama_produk, " ") . '&background=BC41BE&color=F2FF58';
+        return 'https://ui-avatars.com/api/?name=' . trim($this->nama, " ") . '&background=BC41BE&color=F2FF58';
     }
 
     // public function fotoProduk()
