6ee1a161ed5d1fd313db09325df4e2d66be0b2c0
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-01-20 19:45:20 +0700

    temp

--- app/Http/Controllers/API/Member/PesananController.php
+++ app/Http/Controllers/API/Member/PesananController.php
@@ -244,7 +244,7 @@ class PesananController extends Controller
         $data = $member->pesanans;
         $arrFiturTerpilih = [];
 
-        return responseSuccess("Data Pesanan member", $data);
+        return responseSuccess("Data Pesanan member", $data->where('status', 'Selesai'));
 
         if ($request->status_pesanan === 'Draft') {
             $data = $data->first()->where('id_member', $member->id_pengelola)
