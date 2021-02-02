8882bf2a744136674c57f33aeba75c7f25c41121
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-02-02 08:28:22 +0700

    temp

--- resources/views/member/pencarian.blade.php
+++ resources/views/member/pencarian.blade.php
@@ -415,7 +415,7 @@
                                                 // else{
                                                 //     produkItem +='<button type="submit" class="btn fa fa-heart fa-2x fa-responsive cursor-pointer" style="position: absolute;top: 5%; left: 87%; transform: translate(-50%, -50%); -ms-transform: translate(-50%, -50%); background:transparent;"></button>';
                                                 // }
-                                                if($('#fotoProduk').val() != null){
+                                                if(produks['produks'][i].foto_produk[0] != null){
                                                     produkItem +='<img class="card-img-top cursor-pointer" src="'+ produks['produks'][i].foto_produk[0] +'" onclick="window.location.href=" style="height: 180px; border-radius: 10px 10px 0px 0px;" alt="Card image cap"/>';
                                                 }
                                                 else{
