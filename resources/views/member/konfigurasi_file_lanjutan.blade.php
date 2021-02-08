791e0832bdcf278cf000e0fa4f403730db12696b
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-02-08 09:37:35 +0700

    temp

--- resources/views/member/konfigurasi_file_lanjutan.blade.php
+++ resources/views/member/konfigurasi_file_lanjutan.blade.php
@@ -245,7 +245,7 @@
                                         <button class="btn btn-primary-yellow btn-rounded font-weight-bold py-1 px-4 mb-4"
                                             style="border-radius:35px;font-size: 16px;"
                                             @if (!empty($member->konfigurasi) > 1 && $member->konfigurasi != undefined)
-                                                onclick="window.location='{{ route('pencarian', ['id_konfigurasi' => $member->konfigurasi->first()->id_konfigurasi, 'fromKonfigurasi' => 'true'], 'fromEditKonfigurasi' => 'true') }}'"
+                                                onclick="window.location='{{ route('pencarian', ['id_konfigurasi' => $member->konfigurasi->first()->id_konfigurasi, 'fromKonfigurasi' => 'true', 'fromEditKonfigurasi' => 'true']) }}'"
                                             @else
                                                 onclick="window.location='{{ route('pencarian') }}'"
                                             @endif>
