0bd1786f00883aeca9d715ae2e850aa054cd83de
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-02-08 12:11:54 +0700

    temp

--- app/Http/Controllers/KonfigurasiController.php
+++ app/Http/Controllers/KonfigurasiController.php
@@ -176,10 +176,10 @@ class KonfigurasiController extends Controller
 
         $countPage = countPages($pdf);
 
-        if ($request->ubahProduk == 'true' && session()->has('produkKonfigurasiFile')) {
-            $produk = session()->get('produkKonfigurasiFile');
-        } else {
+        if ($request->ubahProduk != 'true') {
             $produk = Produk::find($konfigurasi->id_produk);
+        } else {
+            $produk = session()->get('produkKonfigurasiFile');
         }
 
         return view('member.edit_konfigurasi_file', compact('member', 'konfigurasi', 'produk', 'countPage'));
