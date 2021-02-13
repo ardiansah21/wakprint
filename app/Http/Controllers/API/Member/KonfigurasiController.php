d3ccc546be045e1f7a103f85805473f56677b85c
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-02-13 20:32:39 +0700

    temp

--- app/Http/Controllers/API/Member/KonfigurasiController.php
+++ app/Http/Controllers/API/Member/KonfigurasiController.php
@@ -219,7 +219,7 @@ class KonfigurasiController extends Controller
         }
 
         if ($request->atkTerpilih != null) {
-            $request->atkTerpilih = json_decode($request->atkTerpilih, true);
+            $request->atkTerpilih = json_encode($request->atkTerpilih);
         } else {
             $request->atkTerpilih = [];
         }
