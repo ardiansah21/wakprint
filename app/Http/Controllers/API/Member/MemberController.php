802edc040479fb1852e5eed221b63cb9b83364c4
imaha7 <ilham.maulana.07.0698@gmail.com>
2021-02-14 23:03:00 +0700

    y

--- app/Http/Controllers/API/Member/MemberController.php
+++ app/Http/Controllers/API/Member/MemberController.php
@@ -23,7 +23,7 @@ class MemberController extends Controller
     public function index()
     {
         $produk = Produk::where('status', 'Tersedia')->get();
-        $produk = $produk->sortBy('jarak')->sortByDesc('rating')->sortBy('harga_hitam_putih')->sortBy('harga_berwarna')->take(5)->toArray();
+        $produk = $produk->sortBy('jarak')->sortByDesc('rating')->sortBy('harga_hitam_putih')->sortBy('harga_berwarna')->toArray();
 
         $partner = Pengelola_Percetakan::where('email_verified_at', '!=', null)->get();
 
